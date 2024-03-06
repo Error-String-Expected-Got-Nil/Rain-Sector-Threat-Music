2d0afa96a87f40f8785899fb86a64a1675a3b279
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2024-03-06 09:18:36 -0600

    Basic JSON setup

---
+++ src/data/scripts/plugins/RSTMModPlugin.java
@@ -0,0 +1,6 @@
+package data.scripts.plugins;
+
+import com.fs.starfarer.api.BaseModPlugin;
+
+public class RSTMModPlugin extends BaseModPlugin {
+}
---
+++ src/data/scripts/plugins/RSTM_DebugMode.java
@@ -0,0 +1,7 @@
+package data.scripts.plugins;
+
+import com.fs.starfarer.api.combat.BaseEveryFrameCombatPlugin;
+
+public class RSTM_DebugMode extends BaseEveryFrameCombatPlugin {
+
+}
